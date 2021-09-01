60d52d9b9745b9f0606641940b10ad878c0c33d1
Nancy Zaky <nancyzki@yahoo.com>
2021-09-01 15:47:36 -0700

    commit tree 3bb19a420f69b6c2de36aea6298543669729d552

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+require "pry"
+class Anagram
+attr_accessor :word
+# attr_accessor :arr
+
+  def initialize(word)
+    @word = word
+
+  end
+
+def match(arr)
+  result = []
+  arr.each do |words|
+    if words.chars.sort == @word.chars.sort
+      result << words
+    end
+
+end
+result
+end
+
+end
+
+listen = Anagram.new("listen")
+listen.match(["enlists", "google", "inlets" ,"banana"])
